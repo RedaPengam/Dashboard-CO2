ca9faec04fe40eb56624bbf27f23df7fbd8b91e6
RedaPengam <reda.pengam@etu.imt-lille-douai.fr>
2022-09-22 11:13:41 -0400

    First visu

---
+++ src/components/Board.jsx
@@ -0,0 +1,14 @@
+ 
+export default function Board(props) {
+  return (
+    <div className='board'>
+      <div className='board-header'>
+        <div className='board-header-left'>💻 Ordinateur</div>
+        <div className='board-header-right'>Equivalences 👇</div>
+      </div>
+      <div className='board-content'>
+        <img src="https://assets.website-files.com/604915c3f8006b714c1fa12d/621b8bd083c1454c75d0315c_dashboard%20carbon%20emissions%20management%20software.jpg"/>
+      </div>
+    </div>
+  );
+}
